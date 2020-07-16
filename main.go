23eeff4ab61558287cff44bbcaae167462d5abe2
genndy007 <kochevgenazp@gmail.com>
2020-07-16 21:47:42 +0300

    Developed some basic logic as turns, players, placing X and O

--- main.go
+++ main.go
@@ -12,14 +12,57 @@ var field = [3][3]string{
 	{"7", "8", "9"},
 }
 
+var win bool
+var players = []string{"X", "O"}
+var turnNumber int
+
 const Size = 3
 
 func main() {
-	fmt.Println("Tic-Tac-Goe by Hennadii")
 
-	PrintField()
-	fmt.Scanln()
+	player := players[0]
+	var number string
+	for turnNumber < Size*Size && !win {
+		fmt.Println("Tic-Tac-Goe by Hennadii")
+		turnNumber++
+		PrintField()
+		fmt.Println("Player is ", player)
+		fmt.Print("Enter a number to place: ")
+		fmt.Scanln(&number)
+		field = PlacePlayer(player, number, field)
+		player = ChangePlayer(player)
+		ClearScreen()
+
+	}
+
 	fmt.Println("")
+}
+
+// Places a player
+func PlacePlayer(player, number string, field [3][3]string) [3][3]string {
+	for i := 0; i < Size; i++ {
+		for j := 0; j < Size; j++ {
+			if field[i][j] == number {
+				field[i][j] = player
+			}
+		}
+	}
+	return field
+}
+
+// Changes to X or O
+func ChangePlayer(p string) string {
+	var index int
+	for i := 0; i < len(players); i++ {
+		if p == players[i] {
+			index = i
+		}
+	}
+
+	if index == 0 {
+		return "O"
+	}
+	return "X"
 }
 
 // Print a game field
